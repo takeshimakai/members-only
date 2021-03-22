41e04d0186930659681b2efe6907cfdd171f45c1
Kai Takeshima <takeshimakai@gmail.com>
2021-03-21 20:19:11 -0400

    format timestamp

--- models/post.js
+++ models/post.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
@@ -6,6 +7,12 @@ const PostSchema = new Schema({
   title: { type: String, required: true },
   message: { type: String, required: true },
   timestamp: { type: Date, default: Date.now }
+});
+
+PostSchema
+.virtual('timestampFormatted')
+.get(function() {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('Post', PostSchema);
